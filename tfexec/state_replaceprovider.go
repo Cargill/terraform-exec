47075eb2396651937178170f11330e90441503f2
AnkitaBarbi <som.barbie@gmail.com>
2023-05-22 19:10:01 +0530

    feat: add state replace-provider (#1)

---
+++ tfexec/state_replaceprovider.go
@@ -0,0 +1,87 @@
+package tfexec
+
+import (
+	"context"
+	"os/exec"
+	"strconv"
+)
+
+type stateRepProviderConfig struct {
+	backup      string
+	lock        bool
+	lockTimeout string
+	state       string
+	stateOut    string
+}
+
+var defaultStateRepProviderOptions = stateRepProviderConfig{
+	lock:        true,
+	lockTimeout: "0s",
+}
+
+// stateRepProviderOption represents options used in the Refresh method.
+type stateRepProviderOption interface {
+	configureStateRepProvider(*stateRepProviderConfig)
+}
+
+func (opt *BackupOption) configureStateRepProvider(conf *stateRepProviderConfig) {
+	conf.backup = opt.path
+}
+
+func (opt *LockOption) configureStateRepProvider(conf *stateRepProviderConfig) {
+	conf.lock = opt.lock
+}
+
+func (opt *LockTimeoutOption) configureStateRepProvider(conf *stateRepProviderConfig) {
+	conf.lockTimeout = opt.timeout
+}
+
+func (opt *StateOption) configureStateRepProvider(conf *stateRepProviderConfig) {
+	conf.state = opt.path
+}
+
+func (opt *StateOutOption) configureStateRepProvider(conf *stateRepProviderConfig) {
+	conf.stateOut = opt.path
+}
+
+// StateRepProvider represents the terraform state replace-provider subcommand.
+func (tf *Terraform) StateRepProvider(ctx context.Context, source string, destination string, opts ...stateRepProviderOption) error {
+	cmd, err := tf.stateRepProviderCmd(ctx, source, destination, opts...)
+	if err != nil {
+		return err
+	}
+	return tf.runTerraformCmd(ctx, cmd)
+}
+
+func (tf *Terraform) stateRepProviderCmd(ctx context.Context, source string, destination string, opts ...stateRepProviderOption) (*exec.Cmd, error) {
+	c := defaultStateRepProviderOptions
+
+	for _, o := range opts {
+		o.configureStateRepProvider(&c)
+	}
+
+	args := []string{"state", "replace-provider", "-no-color", "-auto-approve"}
+
+	// string opts: only pass if set
+	if c.backup != "" {
+		args = append(args, "-backup="+c.backup)
+	}
+	if c.lockTimeout != "" {
+		args = append(args, "-lock-timeout="+c.lockTimeout)
+	}
+	if c.state != "" {
+		args = append(args, "-state="+c.state)
+	}
+	if c.stateOut != "" {
+		args = append(args, "-state-out="+c.stateOut)
+	}
+
+	// boolean and numerical opts: always pass
+	args = append(args, "-lock="+strconv.FormatBool(c.lock))
+
+	// positional arguments
+	args = append(args, source)
+	args = append(args, destination)
+
+	return tf.buildTerraformCmd(ctx, nil, args...), nil
+}
---
+++ tfexec/state_replaceprovider_test.go
@@ -0,0 +1,59 @@
+package tfexec
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-exec/tfexec/internal/testutil"
+)
+
+func TestStateRepProviderCmd(t *testing.T) {
+	td := t.TempDir()
+
+	tf, err := NewTerraform(td, tfVersion(t, testutil.Latest_v1))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// empty env, to avoid environ mismatch in testing
+	tf.SetEnv(map[string]string{})
+
+	t.Run("defaults", func(t *testing.T) {
+		stateRepProviderCmd, err := tf.stateRepProviderCmd(context.Background(), "testsource", "testdestination")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertCmd(t, []string{
+			"state",
+			"replace-provider",
+			"-no-color",
+			"-auto-approve",
+			"-lock-timeout=0s",
+			"-lock=true",
+			"testsource",
+			"testdestination",
+		}, nil, stateRepProviderCmd)
+	})
+
+	t.Run("override all defaults", func(t *testing.T) {
+		stateRepProviderCmd, err := tf.stateRepProviderCmd(context.Background(), "testsrc", "testdest", Backup("testbackup"), LockTimeout("200s"), State("teststate"), StateOut("teststateout"), Lock(false))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assertCmd(t, []string{
+			"state",
+			"replace-provide",
+			"-no-color",
+			"-auto-approve",
+			"-backup=testbackup",
+			"-lock-timeout=200s",
+			"-state=teststate",
+			"-state-out=teststateout",
+			"-lock=false",
+			"testsrc",
+			"testdest",
+		}, nil, stateRepProviderCmd)
+	})
+}
